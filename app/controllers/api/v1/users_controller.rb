062d483a24920ee5f38752846322be29af248a22
georgetim1998 <georgetim1998@gmail.com>
2022-10-27 15:40:06 +0300

    user index serialize + filter

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.filter_by_shop(shop_id) if params[:shop_id].present?
+    @users = User.all if params[:shop_id].blank?
+
+    render json: @users
+  end
+
+  private
+
+  def shop_id
+    params[:shop_id].to_i
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,18 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :email
+  has_many :shops do
+    {
+      links: {
+        related: "/api/v1/shops?user_id=#{object.id}"
+      }
+    }
+  end
+
+  has_many :cards do
+    {
+      links: {
+        related: "/api/v1/cards?user_id=#{object.id}"
+      }
+    }
+  end
 end
