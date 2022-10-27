80d679dfeb866dbae067f3c784fd1b7acc203014
georgetim1998 <georgetim1998@gmail.com>
2022-10-27 15:24:45 +0300

    shop index serializer

--- app/controllers/api/v1/shops_controller.rb
+++ app/controllers/api/v1/shops_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::ShopsController < ApplicationController
   def index
-    @shops = Shop.all
+    @shops = Shop.filter_by_card(user_id) if params[:user_id].present?
+    @shops = Shop.all if params[:user_id].blank?
+
     render json: @shops
+  end
+
+  private
+
+  def user_id
+    params[:user_id].to_i
   end
 end
--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -1,5 +1,18 @@
 class ShopSerializer < ActiveModel::Serializer
   attributes :id, :name
-  has_many :users, through: :cards
-  has_many :cards
+  has_many :users do
+    {
+      links: {
+        related: "/api/v1/users?shop_id=#{object.id}"
+      }
+    }
+  end
+
+  has_many :cards do
+    {
+      links: {
+        related: "/api/v1/cards?shop_id=#{object.id}"
+      }
+    }
+  end
 end
