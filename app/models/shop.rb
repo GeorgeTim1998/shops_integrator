6a94a56e0f53f7f3082cdc0bea04d745c481f23c
georgetim1998 <georgetim1998@gmail.com>
2022-10-27 15:28:19 +0300

    shop index filter

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
   has_many :cards, dependent: :destroy
   has_many :users, through: :cards
+
+  scope :filter_by_card, ->(user_id) { joins(:users).where(users: { id: user_id }) }
 end
