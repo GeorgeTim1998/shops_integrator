ceb4ad6b15f661b9967620e6bff5218c1c840b18
georgetim1998 <georgetim1998@gmail.com>
2022-10-29 09:28:34 +0300

    buy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cards, only: %i[index show]
-      resources :shops, only: %i[show update index create] # add buy action
       resources :users, only: %i[show update index create]
+
+      resources :shops, only: %i[show update index create] do
+        member do
+          post :buy
+        end
+      end
     end
   end
 end
