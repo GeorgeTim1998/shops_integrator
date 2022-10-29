6e1d12053c1aab7fa564242c90e5f4245013c06a
georgetim1998 <georgetim1998@gmail.com>
2022-10-29 09:55:46 +0300

    bue action and find card

--- app/controllers/api/v1/shops_controller.rb
+++ app/controllers/api/v1/shops_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ShopsController < ApplicationController
   before_action :find_shop, only: :update
+  before_action :find_card, only: :buy
 
   def index
     @shops = Shop.filter_by_user(user_id) if params[:user_id].present?
@@ -34,7 +35,16 @@ class Api::V1::ShopsController < ApplicationController
     render json: @shop
   end
 
+  def buy
+    byebug
+  end
+
   private
+
+  def find_card
+    byebug
+    @card = User.find(params[:user_id]).cards.where(shop: params[:id]).first
+  end
 
   def user_id
     params[:user_id].to_i
