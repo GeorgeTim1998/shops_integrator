fa18da68702f222bfc2dea078e045a9078d17700
georgetim1998 <georgetim1998@gmail.com>
2022-10-29 18:07:19 +0300

    bue errrors

--- app/controllers/api/v1/shops_controller.rb
+++ app/controllers/api/v1/shops_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::ShopsController < ApplicationController
   before_action :find_shop, only: :update
-  before_action :find_card, only: :buy
 
   def index
     @shops = Shop.filter_by_user(user_id) if params[:user_id].present?
@@ -36,17 +35,17 @@ class Api::V1::ShopsController < ApplicationController
   end
 
   def buy
+    @card = User.find(params[:user_id]).cards.where(shop: params[:id]).first
     @card.add_bonuses(params[:amount])
     amount_due = @card.use_bonuses(params[:amount]) if params[:use_bonuses]
 
     render json: @card, serializer: BuySerializer, success: true, amount_due:
+  rescue NoMethodError, ActiveRecord::RecordNotFound
+    render json: error, adapter: :json,
+           status: :unprocessable_entity
   end
 
   private
-
-  def find_card
-    @card = User.find(params[:user_id]).cards.where(shop: params[:id]).first
-  end
 
   def user_id
     params[:user_id].to_i
@@ -58,5 +57,15 @@ class Api::V1::ShopsController < ApplicationController
 
   def shop_params
     params.require(:shop).permit(:name)
+  end
+
+  def error
+    {
+      success: false,
+      errors: {
+        amount: ['is required'],
+        user_id: ['is required']
+      }
+    }
   end
 end
