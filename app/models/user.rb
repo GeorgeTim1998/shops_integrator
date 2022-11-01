b8ab19db2cfbf08b8f3b34dd5396cb2666e872e4
georgetim1998 <georgetim1998@gmail.com>
2022-11-01 17:21:17 +0300

    users#create unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :cards, dependent: :destroy
   has_many :shops, through: :cards
 
+  validates :email, presence: true
+  validates :email, uniqueness: { scope: :email }
+
   scope :filter_by_shop, ->(shop_id) { joins(:shops).where(shops: { id: shop_id }) }
 
-  validates :email, uniqueness: { scope: :email }
 end
