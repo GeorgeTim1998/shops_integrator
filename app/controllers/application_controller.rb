b030eeadc78c2f2afee572bfe356ac1b34668460
georgetim1998 <georgetim1998@gmail.com>
2022-11-03 13:25:08 +0300

    render_errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+  def error_renderer(errors)
+    data = { errors: [] }
+    errors.each do |error|
+      data[:errors] << error_body(error).merge(meta: error_meta(error))
+    end
+    data
+  end
+end
+
+private
+
+def error_body(error)
+  error_body = I18n.t(:errors)[error.type][:body]
+  {
+    code: error_body[:code],
+    status: error_body[:status],
+    title: error_body[:title],
+    detail: error_body[:detail],
+    source: {
+      pointer: "/data/attributes/#{error.attribute}"
+    }
+  }
+end
+
+def error_meta(error)
+  error_meta = I18n.t(:errors)[error.type][:meta]
+  {
+    attribute: error.attribute,
+    message: error_meta[:message],
+    code: error.type
+  }
 end
