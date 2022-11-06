108910600a1860a5b44937baabf700ee7396b05c
georgetim1998 <georgetim1998@gmail.com>
2022-11-06 10:27:22 +0300

    filtering done

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::CardsController < ApplicationController
   def index
     @cards = Card.all
-    filtering_params.each do |key, value|
-      @cards = @cards.public_send("filter_by_#{key}", value) if value.present?
+    filtering_params.each do |_key, hash|
+      hash.each do |key, value|
+        @cards = @cards.public_send("filter_by_#{key}", value) if value.present?
+      end
     end
 
     @cards = @cards.to_a
 
-    filtering_params_bonuses.each do |_key, value|
-      if value == 'sum' && @cards.present?
-        render jsonapi: @cards, meta: { stats: { bonuses: { sum: @cards.first.all_cards_bonuses(@cards) } } } and return
+    filtering_params_bonuses.each do |_key, hash|
+      hash.each do |_key, value|
+        if value == 'sum' && @cards.present?
+          render jsonapi: @cards, meta: { stats: { bonuses: { sum: @cards.first.all_cards_bonuses(@cards) } } } and return
+        end
       end
     end
 
@@ -25,10 +29,10 @@ class Api::V1::CardsController < ApplicationController
   private
 
   def filtering_params
-    params.slice(:filter)[:filter]
+    params.slice(:filter)
   end
 
   def filtering_params_bonuses
-    params.slice(:stats)[:stats]
+    params.slice(:stats)
   end
 end
